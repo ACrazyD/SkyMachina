b80b825580fd1f2e9f9cff3d502134be9f1ef86d
AcrazyD <41309569+AcrazyD@users.noreply.github.com>
2022-12-03 11:35:01 -0800

    Adding Function;s for InWorld Crafting

---
+++ kubejs/server_scripts/in_world.js
@@ -0,0 +1,28 @@
+//priority 0
+
+onEvent('block.right_click', e => {
+    //player.give("minecraft:andesite")
+    function inWorldCrafting(item, hi, output, chance) {
+        if (e.hand != MAIN_HAND) {
+            if (e.item.id === item && e.block.id === hi) {
+                e.player.swingArm(Hand.OFF_HAND)
+                e.player.damageHeldItem(Hand.OFF_HAND, damage);
+                e.player.give(output);
+                 if (Math.random() * 100 > chance) {
+                    e.server.runCommandSilent(`/setblock ${e.block.x} ${e.block.y} ${e.block.z} minecraft:air`)
+                }
+            }
+            return;
+        }
+        if (e.item.id === item && e.block.id === hi) {
+            e.player.swingArm(Hand.MAIN_HAND);
+            e.player.damageHeldItem(Hand.MAIN_HAND, damage);
+            e.player.give(output);
+             if (Math.random() * 100 > chance) {
+                e.server.runCommandSilent(`/setblock ${e.block.x} ${e.block.y} ${e.block.z} minecraft:air`)
+            }
+        }
+    }
+
+    inWorldCrafting("tconstruct:sledge_hammer", "minecraft:andesite", "create:andesite_alloy", 75, 1)
+})
