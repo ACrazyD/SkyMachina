de9f083b827f794a1eabf1f57f3fc8d1b279bc9f
AcrazyD <41309569+AcrazyD@users.noreply.github.com>
2022-12-09 22:07:50 -0800

    Loads of New Items - Prepping for Large Updates...

--- kubejs/server_scripts/mods/tinkers.js
+++ kubejs/server_scripts/mods/tinkers.js
@@ -1,7 +1,111 @@
 //priority 0
-console.info('Sifty Sifty...')
+console.info('Smelty Smelty.')
+
+////CASTING TABLE////
+global.casingTable = (event, cast, consumed, outputItem, inputFluid, amount, cooling, ID) => {
+    event.custom({
+      type: 'tconstruct:casting_table',
+      cast: {
+        tag: cast
+      },
+      cast_consumed: consumed,
+      fluid: {
+        tag: inputFluid,
+        amount: amount
+      },
+      result: outputItem,
+      cooling_time: cooling
+  }).id(ID)}
+
+  ////CASTING BASIN////
+global.casingBasin = (event, outputItem, inputFluid, amount, cooling, ID) => {
+  event.custom({
+    type: 'tconstruct:casting_basin',
+    fluid: {
+      tag: inputFluid,
+      amount: amount
+    },
+    result: outputItem,
+    cooling_time: cooling
+}).id(ID)}
+
+////CASTING BASIN WITH CAST////
+global.casingBasinCast = (event, cast, consumed, outputItem, inputFluid, amount, cooling, ID) => {
+event.custom({
+  type: 'tconstruct:casting_basin',
+  cast: {
+    tag: cast
+  },
+  cast_consumed: consumed,
+  fluid: {
+    tag: inputFluid,
+    amount: amount
+  },
+  result: outputItem,
+  cooling_time: cooling
+}).id(ID)}
+
+//////// ORE MELTING /////////
+global.tinkersOreMelting = (event, outputFluid, amount, byproductFluid, byproductAmount, inputItem, temperature, time, ID) => {
+    event.custom({
+        type: 'tconstruct:ore_melting',
+        rate: 'metal',
+        ingredient: {
+            tag: inputItem
+        },
+        result: {
+          fluid: outputFluid, 
+          amount: amount //180
+        },
+        temperature: temperature, //700
+        time: time, //142
+        byproducts: [
+          {
+            fluid: byproductFluid,
+            amount: byproductAmount
+          }
+        ]
+      
+}).id(ID)}
+
+//////// ORE MELTING /////////
+global.tinkersMelting = (event, outputFluid, amount, byproductFluid, byproductAmount, inputItem, temperature, time, ID) => {
+    event.custom({
+        type: 'tconstruct:melting',
+        ingredient: {
+            item: inputItem
+        },
+        result: {
+          fluid: outputFluid, 
+          amount: amount //180
+        },
+        temperature: temperature, //700
+        time: time, //142
+        byproducts: [
+          {
+            fluid: byproductFluid,
+            amount: byproductAmount
+          }
+        ]
+
+}).id(ID)}
+//////// ALLOYING /////////
+global.tinkersAlloying = (event, outputFluid, amount, inputFluids, temperature, ID) => {
+    event.custom({
+    type: 'tconstruct:alloy',
+    inputs: inputFluids,
+    result: {
+      fluid: outputFluid,
+      amount: amount
+    },
+    temperature: temperature
+}).id(ID)}
+
+
+
 
 onEvent('recipes', e => {
+    const { casting_table, casting_basin } = e.recipes.tconstruct;
 
     function tinkersMeltingSingle(input, output, amount, temp, time) {
         e.custom({
@@ -25,14 +129,22 @@ onEvent('recipes', e => {
         })
     }
 
+
     //End of Functions
 
     //Tinkers Melting
     tinkersMeltingSingle('minecraft:andesite', 'kubejs:liquid_andesite', 115, 500, 60)
 
     //Tinkers Alloy
+    /*
+    global.tinkersAlloying(event, item.fluid_id, (item.amount*90), item.tinkers_input, 1000, `skytweaks:smeltery/alloys/molten_${item.material}`)
+        'tinkers_input': [{tag: 'forge:molten_copper',amount: 180},{tag: 'forge:molten_zinc',amount: 90}],
+    */
+
     tinkersAlloy2in('forge:liquid_andesite', 50, 'forge:molten_zinc', 50, 'kubejs:molten_andesite_alloy', 100)
 
+    //Tinkers Casting
+    casting_table('create:andesite_alloy', 'kubejs:molten_andesite_alloy', 90).cast().coolingTime(180)
 
 
 
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,8 +3,57 @@
 console.info('Making Dreams, well... Making Items, Fluids, and Blocks real! (Tell they are added to SkyMachinaTweaks)')
 
 onEvent('item.registry', e => {
+	
+	//Replacement For RSTM Which will be removed Soon...
+	//Circuitry
+	e.create('circuit_basic').displayName('Basic Circuitry')
+	e.create('circuit_basic_incomplete','create:sequenced_assembly').displayName('Incomplete Basic Circuitry')
+	e.create('circuit_enhanced').displayName('Enhanced Circuitry')
+	e.create('circuit_enhanced_incomplete','create:sequenced_assembly').displayName('Incomplete Basic Enhanced')
+	e.create('circuit_advanced').displayName('Advanced Circuitry')
+	e.create('circuit_advanced_incomplete','create:sequenced_assembly').displayName('Incomplete Basic Advanced')
+	e.create('circuit_industrial_grade').displayName('Industrial Grade Circuitry')
+	e.create('circuit_industrial_grade_incomplete','create:sequenced_assembly').displayName('Incomplete Industrial Grade Circuitry')
+	e.create('circuit_overclocked').displayName('Overclocked Circuitry')
+	e.create('circuit_overclocked_incomplete','create:sequenced_assembly').displayName('Incomplete Overclocked Circuitry')
+	e.create('circuit_reliable').displayName('Reliable Circuitry')
+	e.create('circuit_reliable_incomplete','create:sequenced_assembly').displayName('Incomplete Reliable Circuitry')
+	e.create('circuit_avionics').displayName('Avionics circuitry')
+	e.create('circuit_avionics_incomplete','create:sequenced_assembly').displayName('Incomplete Avionics Circuitry')
+	e.create('circuit_capacitive').displayName('Capacitive Circuitry')
+	e.create('circuit_capacitive_incomplete','create:sequenced_assembly').displayName('Incomplete Capacitive Circuitry')
+	//Foils
+	e.create('foil_copper').displayName('Copper Foil')
+	e.create('foil_copper_incomplete','create:sequenced_assembly').displayName('Incomplete Copper Foil')
+	e.create('foil_iron').displayName('Iron Foil')
+	e.create('foil_iron_incomplete','create:sequenced_assembly').displayName('Incomplete Iron Foil')
+	e.create('foil_steel').displayName('Steel Foil')
+	e.create('foil_steel_incomplete','create:sequenced_assembly').displayName('Incomplete Steel Foil')
+	e.create('foil_aluminum').displayName('Aluminium Foil')
+	e.create('foil_aluminum_incomplete','create:sequenced_assembly').displayName('Incomplete Aluminium Foil')
+	//Coils
+	e.create('coil_empty').displayName('Empty Coil')
+	e.create('coil_copper').displayName('Copper Coil')
+	e.create('coil_gold').displayName('Gold Coil')
+	e.create('coil_aluminum').displayName('Aluminium Coil')
+	//Misc Parts
+	e.create('motor_kinetic').displayName('Kinetic Motor')
+	e.create('motor_electrostatic').displayName('Electrostatic Motor')
+	e.create('motor_brushless').displayName('Brushless Motor')
+
+
+
 
 })
+
+/*
+e.create('circuit_').displayName('')
+e.create('circuit__incomplete','create:sequenced_assembly').displayName('Incomplete ')
+e.create('').displayName('')
+e.create('foil_').displayName('')
+		e.create('foil__incomplete','create:sequenced_assembly').displayName('Incomplete ')
+*/
+
 
 onEvent('block.registry', e => {
 	
